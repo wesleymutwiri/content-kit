b5a1ed11fbc557fe84ae17101ff32617a72c82ac
wesleymutwiri <wesleymochiemo@gmail.com>
2022-03-16 17:07:51 +0300

    slightly switched config to fix error

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,25 @@
-import adapter from '@sveltejs/adapter-netlify';
+const sveltePreprocess = require('svelte-preprocess');
+const netlify = require('@sveltejs/adapter-netlify');
+const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+module.exports = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: adapter({
-			split: false
-		})
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: netlify(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {})
+			}
+		}
 	}
 };
-
-export default config;
